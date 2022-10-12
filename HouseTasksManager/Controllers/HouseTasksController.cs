3ea7b67be436b3f7f1937bdc1efd887f38a85086
LAPTOP-9RFM1750\javie <javier.bascu00@gmail.com>
2022-10-12 17:19:44 +0200

    Fixed Tasks Value display

--- HouseTasksManager/Controllers/HouseTasksController.cs
+++ HouseTasksManager/Controllers/HouseTasksController.cs
@@ -86,7 +86,7 @@ namespace HouseTasksManager.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Description")] HouseTask houseTask)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Description,Value")] HouseTask houseTask)
         {
             if (id != houseTask.Id)
             {
